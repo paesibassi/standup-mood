70b5840186463147864a47e8611b757772066e20
Federico Calore <Federico.Calore@ing.com>
2022-04-15 11:23:29 +0200

    Use service account keys instead of oAuth2 token

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -9,18 +8,14 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"gitlab.com/wbaa-experiments/standup-mood/controller"
-	"gitlab.com/wbaa-experiments/standup-mood/oauth"
 	"gitlab.com/wbaa-experiments/standup-mood/secrets"
 	"gitlab.com/wbaa-experiments/standup-mood/spreadsheets"
-	"golang.org/x/oauth2"
 )
-
-var getNewTokenFlow *bool = flag.Bool("t", false, "Retrieve a new token from Google Sheets")
 
 func main() {
 	flag.Parse()
 
-	err := setupSpreadsheetsService(*getNewTokenFlow)
+	err := setupSpreadsheetsService()
 	if err != nil {
 		log.Fatalf("could not complete setup: %v\n", err)
 	}
@@ -36,39 +31,12 @@ func main() {
 	r.Run(":" + port)
 }
 
-func setupSpreadsheetsService(getNewTokenFlow bool) error {
-	creds, err := secrets.SecretFromGCloud("app-credentials")
+func setupSpreadsheetsService() error {
+	creds, err := secrets.SecretFromGCloud("sheets-service-account")
 	if err != nil {
 		return fmt.Errorf("unable to get app credentials: %v", err)
 	}
-	config, err := oauth.GetConfig(creds)
-	if err != nil {
-		return fmt.Errorf("unable to parse client secret to config: %v", err)
-	}
-	tokenBytes, err := secrets.SecretFromGCloud("sheets-token")
-	if err != nil {
-		return fmt.Errorf("unable to get token from secrets: %v", err)
-	}
-
-	var token *oauth2.Token
-	if getNewTokenFlow || tokenBytes == nil {
-		token, err = oauth.GetTokenFromWeb(config) // this would only work locally
-		if err != nil {
-			return fmt.Errorf("unable to get token from web oauth flow: %v", err)
-		}
-		tokenBytes, err = json.Marshal(token)
-		if err != nil {
-			return fmt.Errorf("unable to marshal token to bytes array: %v", err)
-		}
-		secrets.AddSecretVersion("sheets-token", tokenBytes)
-	} else {
-		token, err = oauth.TokenFromBytes(tokenBytes)
-		if err != nil {
-			return fmt.Errorf("unable to unmarshal token bytes: %v", err)
-		}
-	}
-	client := oauth.GetClient(config, token)
-	ss, err := spreadsheets.NewService(client)
+	ss, err := spreadsheets.NewService(creds)
 	if err != nil {
 		return fmt.Errorf("unable to retrieve Sheets client: %v", err)
 	}
