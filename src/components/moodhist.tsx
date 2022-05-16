8d08f2a8b248d6947bed0a4fe68a25a705e31887
Federico Calore <Federico.Calore@ing.com>
2022-05-16 22:44:02 +0200

    Add avgmood text on sparkline and cleanup

--- src/components/moodhist.tsx
+++ src/components/moodhist.tsx
@@ -1,43 +1,27 @@
 import React, { FC } from 'react';
 import Sparkline, { DateValue } from './visualization/sparkline';
-import { range } from "d3";
 
 type Props = {
-    memberHistory?: DateValue[];
+  memberHistory?: DateValue[];
 };
-
-// can be removed when api is implemented
-const genData = (): DateValue[] => {
-  const data: DateValue[] = range(20, 1, -1).map((n) => {
-      const date = new Date(new Date().setDate(new Date().getDate() - n))
-      .toISOString()
-      .substring(0, 10);
-      const value = Math.max(Math.ceil(Math.random() * 10) / 2 - 1.5, 1);
-
-      return { date, value };
-  });
-
-  return data;
-};
-
 
 const MoodHistory: FC<Props> = ({ memberHistory }: Props) => {
   // TODO replace with something implemented from backend.
-    const historyLength = 30;
-    const minDate = new Date(new Date().setDate(new Date().getDate() - historyLength))
-      .toISOString()
-      .substring(0, 10);
+  const historyLength = 30;
+  const minDate = new Date(new Date().setDate(new Date().getDate() - historyLength))
+    .toISOString()
+    .substring(0, 10);
 
-    // if (memberHistory === undefined) {
-    //   return (
-    //     <div className="border rounded text-center" style={{ width: 80, height: 30 }}>
-    //       &#8211;&and;&#8212;&or;&#8211;
-    //     </div>
-    //   );
-    // }
+  if (memberHistory === undefined) {
     return (
-      <Sparkline minDate={minDate} width={80} height={30} data={genData()} />
+      <div className="border rounded text-center" style={{ width: 80, height: 30 }}>
+        &#8211;&and;&#8212;&or;&#8211;
+      </div>
     );
+  }
+  return (
+    <Sparkline minDate={minDate} width={80} height={30} data={memberHistory} />
+  );
 };
 
 export default MoodHistory;
--- src/components/visualization/sparkline.tsx
+++ src/components/visualization/sparkline.tsx
@@ -1,98 +1,107 @@
 import React, { FC, useMemo } from 'react';
 import {
- curveBasis, interpolateCool, line, mean, range, scaleLinear, scaleTime,
+  curveBasis, interpolateCool, line, mean, range, scaleLinear, scaleTime,
 } from 'd3';
 import './sparkline.css';
 
 export interface DateValue {
-    date: string;
-    value: number;
+  date: string;
+  value: number;
 }
 
 type Props = {
-    width: number;
-    height: number;
-    data: DateValue[];
-    minDate: string;
+  width: number;
+  height: number;
+  data: DateValue[];
+  minDate: string;
 };
 
 const Sparkline: FC<Props> = ({
- width, height, data, minDate,
+  width, height, data, minDate,
 }: Props) => {
-    const yPadding = 0.1;
-    const xPadding = 0.05;
+  const yPadding = 0.1;
+  const xPadding = 0.05;
 
-    const timeScale = scaleTime()
-        .domain([new Date(minDate), new Date()])
-        .range([width * xPadding, width * (1 - xPadding)])
-        .clamp(true);
+  const timeScale = scaleTime()
+    .domain([new Date(minDate), new Date()])
+    .range([width * xPadding, width * (1 - xPadding)])
+    .clamp(true);
 
-    const yScale = scaleLinear()
-        .domain([1, 5])
-        .range([height * (1 - yPadding), yPadding * height])
-        .clamp(true);
+  const yScale = scaleLinear()
+    .domain([1, 5])
+    .range([height * (1 - yPadding), yPadding * height])
+    .clamp(true);
 
-    const pathContext = useMemo(
-        () => line<DateValue>()
-            .curve(curveBasis)
-            .x((d) => timeScale(new Date(d.date)))
-            .y((d) => yScale(d.value)),
-        [timeScale, yScale],
-    );
+  const pathContext = useMemo(
+    () => line<DateValue>()
+      .curve(curveBasis)
+      .x((d) => timeScale(new Date(d.date)))
+      .y((d) => yScale(d.value)),
+    [timeScale, yScale],
+  );
 
-    if (!data || data.length === 0) return null;
+  if (!data || data.length === 0) return null;
 
-    const avgMood = mean(data.map((d) => d.value)) as number;
-    const lastDataPoint = data[data.length - 1];
-    const avgSignDiffers = Math.abs(avgMood - 3) > 0.5;
+  const avgMood = mean(data.map((d) => d.value)) as number;
+  const lastDataPoint = data[data.length - 1];
+  const avgSignDiffers = Math.abs(avgMood - 3) > 0.5;
 
-    return (
-      <svg
-        viewBox={`0 0 ${width} ${height}`}
-        width={width}
-        height={height}
-      >
-        <defs>
-          <linearGradient id="gradient" x1="0%" x2="0%" y1="100%" y2="0%" gradientUnits="userSpaceOnUse">
-            {range(10).map((i) => (
-              <stop
-                key={i}
-                offset={`${(i / 10) * 100}%`}
-                stopColor={interpolateCool(i / 14)}
-              />
-            ))}
-          </linearGradient>
-        </defs>
-        <g>
-          <rect className="sparkline-bg" />
-          <line 
-            x1={timeScale.range()[0]} 
-            x2={timeScale.range()[1]} 
-            y1={yScale(3)} 
-            y2={yScale(3)} 
-            className="sparkline-mid-line"
-          />
-          { avgSignDiffers && <line 
-            x1={timeScale.range()[0]} 
-            x2={timeScale.range()[1]} 
-            y1={yScale(avgMood)} 
-            y2={yScale(avgMood)} 
+  return (
+    <svg
+      viewBox={`0 0 ${width} ${height}`}
+      width={width}
+      height={height}
+    >
+      <defs>
+        <linearGradient id="gradient" x1="0%" x2="0%" y1="100%" y2="0%" gradientUnits="userSpaceOnUse">
+          {range(10).map((i) => (
+            <stop
+              key={i}
+              offset={`${(i / 10) * 100}%`}
+              stopColor={interpolateCool(i / 14)}
+            />
+          ))}
+        </linearGradient>
+      </defs>
+      <g>
+        <rect className="sparkline-bg" />
+        <line
+          x1={timeScale.range()[0]}
+          x2={timeScale.range()[1]}
+          y1={yScale(3)}
+          y2={yScale(3)}
+          className="sparkline-mid-line"
+        />
+        {avgSignDiffers && (
+          <line
+            x1={timeScale.range()[0]}
+            x2={timeScale.range()[1]}
+            y1={yScale(avgMood)}
+            y2={yScale(avgMood)}
             className="sparkline-avg-line"
-          />}
-          <path
-            className="sparkline-line"
-            d={(data && pathContext(data)) || ''}
-            stroke="url(#gradient)"
           />
-          <circle
-            className="sparkline-point"
-            cx={timeScale(new Date(lastDataPoint.date))}
-            cy={yScale(lastDataPoint.value)}
-            fill="url(#gradient)"
-          />
-        </g>
-      </svg>
-    );
+        )}
+        <path
+          className="sparkline-line"
+          d={(data && pathContext(data)) || ''}
+          stroke="url(#gradient)"
+        />
+        <circle
+          className="sparkline-point"
+          cx={timeScale(new Date(lastDataPoint.date))}
+          cy={yScale(lastDataPoint.value)}
+          fill="url(#gradient)"
+        />
+        <text
+          x="70%"
+          y="80%"
+          className="fs-6 fw-lighter"
+        >
+          {avgMood.toFixed(1)}
+        </text>
+      </g>
+    </svg>
+  );
 };
 
 export default Sparkline;
